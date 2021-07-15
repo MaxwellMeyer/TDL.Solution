9f7a408d201c4457450b30dddfa68b838949f452
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-14 22:05:15 -0700

    add static var to store Items and static method to return Items

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,11 +2,18 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; }
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> { };
 
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
     }
   }
+
 }
