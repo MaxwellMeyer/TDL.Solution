bd781595d35999439d94a42a56f08cc00c8d6250
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-26 18:42:17 -0700

    update Items model to utilize db connections

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,32 +1,54 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
-  public class Item
+  namespace ToDoList.Models
   {
-    public string Description { get; set; }
-    public int Id { get; }
-    private static List<Item> _instances = new List<Item> { };
-    public Item(string description)
+    public class Item
     {
-      Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
-    }
+      public string Description { get; set; }
+      public int Id { get; }
 
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId - 1];
-    }
+      public Item(string description, int id)
+      {
+        Description = description;
+        Id = id;
+      }
+
+      public static List<Item> GetAll()
+      {
+        List<Item> allItems = new List<Item> { };
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT * FROM items;";
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        while (rdr.Read())
+        {
+          int itemId = rdr.GetInt32(0);
+          string itemDescription = rdr.GetString(1);
+          Item newItem = new Item(itemDescription, itemId);
+          allItems.Add(newItem);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+        return allItems;
+      }
+      public static void ClearAll()
+      {
+
+      }
+      public static Item Find(int searchId)
+      {
+        // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with database.
+        Item placeholderItem = new Item("placeholder item");
+        return placeholderItem;
+      }
 
 
+    }
   }
-}
