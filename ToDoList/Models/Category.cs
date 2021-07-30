a19fe8dfa2aa00170af06f1f09804d7e40824419
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-29 20:05:29 -0700

    add create methods to ItemsController

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
-using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
   public class Category
   {
-    private static List<Category> _instances = new List<Category> { };
+    private static List<Category> _instances = new List<Category> {};
     public string Name { get; set; }
     public int Id { get; }
     public List<Item> Items { get; set; }
@@ -15,12 +14,7 @@ namespace ToDoList.Models
       Name = categoryName;
       _instances.Add(this);
       Id = _instances.Count;
-      Items = new List<Item> { };
-    }
-    public Category(string name, int id)
-    {
-      Name = name;
-      Id = id;
+      Items = new List<Item>{};
     }
 
     public static void ClearAll()
@@ -30,30 +24,12 @@ namespace ToDoList.Models
 
     public static List<Category> GetAll()
     {
-      List<Category> allCategories = new List<Category> { };
-      MySqlConnection conn = DB.Connection();
-      conn.Open();
-      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"SELECT * FROM categories;";
-      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-      while (rdr.Read())
-      {
-        int categoryId = rdr.GetInt32(0);
-        string categoryDescription = rdr.GetString(1);
-        Category newCategory = new Category(categoryDescription, categoryId);
-        allCategories.Add(newCategory);
-      }
-      conn.Close();
-      if (conn != null)
-      {
-        conn.Dispose();
-      }
-      return allCategories;
+      return _instances;
     }
 
     public static Category Find(int searchId)
     {
-      return _instances[searchId - 1];
+      return _instances[searchId-1];
     }
 
     public void AddItem(Item item)
