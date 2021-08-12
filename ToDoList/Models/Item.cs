4f596aa228fee862d85fb52d0290b0d97ca40280
Maxwell Meyer <maxreadswell@gmail.com>
2021-08-11 19:06:16 -0700

    update items model/controller

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -11,8 +11,8 @@ namespace ToDoList.Models
 
         public int ItemId { get; set; }
         public string Description { get; set; }
-        public bool Status { get; set; }
+        public virtual ApplicationUser User { get; set; } 
 
-        public virtual ICollection<CategoryItem> JoinEntities { get;}
+        public ICollection<CategoryItem> JoinEntities { get;}
     }
 }
